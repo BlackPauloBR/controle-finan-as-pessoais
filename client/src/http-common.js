a7a27e2773f4c949fc3a3e944555efa4b6effa5b
BlackPauloBR <pauloroberto_93@live.com>
2020-07-23 03:18:03 -0300

    ajustes baseURL

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-
 export default axios.create({
-  baseURL: `${BASE_URL}`,
+  baseURL: BASE_URL,
   headers: {
     'Content-type': 'application/json',
   },
