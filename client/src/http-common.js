b5b939baf77a9b45838e39f1cde4719884949972
BlackPauloBR <pauloroberto_93@live.com>
2020-07-22 22:09:19 -0300

    corrigido para https

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
-export const REACT_APP_API_URL = `http://black-financas-pessoais.herokuapp.com/:${process.env.PORT}/api/transaction`;
+export const REACT_APP_API_URL = `https://black-financas-pessoais.herokuapp.com/:${process.env.PORT}/api/transaction`;
 
 export default axios.create({
   baseURL: REACT_APP_API_URL,
