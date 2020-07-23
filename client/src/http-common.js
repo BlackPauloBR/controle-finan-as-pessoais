28b2ac2f0d598aee3d86203328909876433cbfd2
BlackPauloBR <pauloroberto_93@live.com>
2020-07-23 02:42:26 -0300

    alteração em axios

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL:
-    `http://localhost:${process.env.PORT}/api/transaction` ||
-    `https://black-financas-pessoais.herokuapp.com/api/transaction`,
+  baseURL: `https://black-financas-pessoais.herokuapp.com/api/transaction`,
   headers: {
     'Content-type': 'application/json',
   },
